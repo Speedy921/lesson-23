8935d01dc5b1420a87457f2caa40ae478666929a
Artem Viatkin <newyorkbox47@gmail.com>
2023-01-21 12:17:16 +0200

    Добавьте файлы проекта.

---
+++ lesson 23/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_23
+{
+    public abstract class CloneableType
+    {
+        //поддерживать этот "полиморфный интерфейс"
+        //могут только произведные типа
+        //классы в других иерархиях не имют доступа
+        //к данному абстрактному члену
+        public abstract object Clone();
+
+    }
+}
---
+++ lesson 23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("****A First Look at Interfaces****\n");
+CloneableExample();
+
+static void CloneableExample()
+{
+    //все эти классы поддерживют интерфейс ICloneable
+    string myStr = "Hello";
+    OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+
+    //все они могут быть переданы методу
+    //применяющему параметр типа ICloneable
+    CloneMe(myStr);
+    CloneMe(unixOS);
+
+    static void CloneMe(ICloneable c)
+    {
+        //клонирвать то, что получено и вывести имя
+        object theClone = c.Clone();
+        Console.WriteLine("YOur clone is a: {0}", theClone.GetType().Name);
+    }
+}
